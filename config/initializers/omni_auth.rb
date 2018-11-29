7c2a36a59f6a7cf248ce85baa5e65799827c60b1
Yoshiyuki Hirano <yhirano@me.com>
2018-11-29 16:06:01 +0900

    Configure OmniAuth

---
+++ config/initializers/omni_auth.rb
@@ -0,0 +1,13 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :facebook,
+    Rails.application.credentials.facebook['api_key'],
+    Rails.application.credentials.facebook['api_secret']
+  )
+
+  provider(
+    :twitter,
+    Rails.application.credentials.twitter['api_key'],
+    Rails.application.credentials.twitter['api_secret']
+  )
+end
